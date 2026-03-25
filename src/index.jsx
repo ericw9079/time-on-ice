afa6498e578c1d5f91d21d29f9288393a1d6f376
Eric <minecraftisawsome007+github@gmail.com>
2026-03-25 19:44:48 -0400

    refactor: Expose app version + version bump

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import packageJson from '../package.json';
+
+window.appVersion = packageJson.version;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
